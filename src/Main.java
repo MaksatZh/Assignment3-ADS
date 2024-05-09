0533dffeab839daefb3bad485e6eee65c13a17b8
MaksatZh <230333@astanait.edu.kz>
2024-05-09 21:36:52 +0500

    add binary search tree

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+    MyBinarySearchTree tree = new MyBinarySearchTree();
+    tree.insert(95);
+    tree.insert(20);
+    tree.insert(50);
+    tree.insert(40);
+    tree.insert(90);
+    tree.insert(60);
+    tree.insert(70);
+
+
+    System.out.print(tree);
+
+
+
+
+    }
+}
